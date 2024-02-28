57b3b635368f45fba70554b18887be0814b2866f
devosema <benaliosama3@gmail.com>
2024-02-29 00:20:00 +0100

    fix bugs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,14 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 
 //Init DB
 const firebaseConfig = {
-    apiKey: "AIzaSyDLTBQxEZH-fzdoDKqImdKLHNRTZZhQUYA",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "whatsapp-clone2-6da71.firebaseapp.com",
     projectId: "whatsapp-clone2-6da71",
     storageBucket: "whatsapp-clone2-6da71.appspot.com",
     messagingSenderId: "833555321773",
     appId: "1:833555321773:web:53d160b2ab5105b8278724"
 };
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore(app);
